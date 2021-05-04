8a8d3e100b9ce50cab558d4a626fcbba022d26a1
glms2020 <83287446+glms2020@users.noreply.github.com>
2021-05-04 23:27:19 +0100

    Update GLMS_Employment_Rate_levels2.R

--- GLMS_Employment_Rate_levels2.R
+++ GLMS_Employment_Rate_levels2.R
@@ -3,10 +3,6 @@ library("reshape2")
 library("rlang")
 
 
-#----Versioning----
-
-# 26/03/2019 updated code where weight<-PWT, to select latest variable available.
-
 #----Employment rate by demographic:contents-----
 
 #1.Headline_stats_function finds employment rates, samples sizes and populations
@@ -125,7 +121,7 @@ library("rlang")
   
   assign(paste0("Headline_Q",quarter,"_",year),headline_stats,envir = globalenv())
   
-  #write.csv(headline_stats,paste0("Outputs for GLMS/Headline_Q",quarter,"_",year,".csv"))
+  #write.csv(headline_stats,paste0("Outputs_folder/Headline_Q",quarter,"_",year,".csv"))
   
 }
 
@@ -398,10 +394,10 @@ Graduate_breakdown_year<-function(year){
   
 
   write.csv(output,
-            paste0(filepath, "Outputs_for_GLMS/Graduate_breakdown_lev_", year, ".csv"))
+            paste0(filepath, "Outputs_folder/Graduate_breakdown_lev_", year, ".csv"))
   
   saveRDS(object = output,
-          file = paste0(filepath, "Outputs_for_GLMS/EES_rds/Graduate_breakdown_lev_", year, ".rds"))
+          file = paste0(filepath, "Outputs_folder/Graduate_breakdown_lev_", year, ".rds"))
   
  
   
