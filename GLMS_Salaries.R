7350149291aeba12e4346a388ae86598485b6c23
glms2020 <83287446+glms2020@users.noreply.github.com>
2021-05-04 22:53:19 +0100

    Add files via upload

---
+++ GLMS_Salaries.R
@@ -0,0 +1,362 @@
+
+#----Versioning----
+
+# 26/03/2019 updated code where weight<-PIWT, to select latest variable available.
+#09/04/2019 updated to add time series for salaries by gender, and by age and gender
+#09/04/2019 updated to add graduate breakdown of salaries by industry and gender
+
+
+#----Employment rate by demographic----
+
+
+#Find the median, lower and upper quartiles of salaries by demographic characteristic, and reshape (melt) data to have the characteristic as a variable rather than 
+#column name
+
+#Note this gives different answers to those in the 2016 publication due to a different methodology to calculate the percentiles.
+#The method used in SPSS code (using CTABLES) to calcualte percentiles was not the best method: http://www-01.ibm.com/support/docview.wss?uid=swg21480663
+#This instead uses a weighted average method to provide an unbiased estimate. (as in method 2 here: https://www.xycoon.com/quartiles.htm)
+
+Average_salary_by<-function(dataset,demographic){
+  #find the weight variable
+  weight<-tail(sort(names(dataset[grep("PIWT", names(dataset))])),1)
+
+  group_by_at  
+ 
+  dataset%>%
+    group_by_at(demographic)%>%
+    dplyr::summarise(p25=weighted.quantile(GRSSWK, !!sym(weight), probs=0.25),
+                     median=weighted.median(GRSSWK,!!sym(weight)),
+                     p90=weighted.quantile(GRSSWK, !!sym(weight), probs=0.90),
+                     mean=weighted.mean(GRSSWK,!!sym(weight)),
+                     sample=n(),
+                     population=sum(!!sym(weight)))%>% 
+    as.data.frame()%>%  
+    mutate(variable=paste0(demographic, collapse = "_"))
+  
+  #take a dataset; based on the demographic characeristic separate the dataet into groups; find the quartiles and median of GRSSWK (gross weekly pay) and weight on PIWT16
+  #reshape dataset for ease of output- turn the demographic into a variable rather than a column heading
+}
+
+
+#Note: %>% is the 'piping' operator used in the dplyr package, it passes the previous argument to the next function. So the above could be re-written as:
+#temp_dataset<-group_by(dataset, AgeGroup)
+#temp2_dataset<-dplyr::summarise(temp_dataset,p25=weighted.quantile(GRSSWK, PIWT16, probs=0.25),
+#                      median=weighted.median(GRSSWK,PIWT16),
+#                      p75=weighted.quantile(GRSSWK, PIWT16, probs=0.75))
+#age<-melt(temp2_dataset,id=c("p25","median","p75"))
+
+# where temporary datasets are used to pass the data to the next function instead of %>%
+
+#restructure dataset so that 'ageband' is a variable in each row, rather than a header: 
+#this will allow us to bind together multiple datasets as they'll have the same column headings
+
+
+
+Average_Salaries<-function(year){
+  
+  #select dataset: for example if quarter =1 and year =2016 this will get Q1_2016 and set this to 'dataset'
+  dataset1<-get(paste0("Q1_",year))
+  dataset2<-get(paste0("Q2_",year))
+  dataset3<-get(paste0("Q3_",year))
+  dataset4<-get(paste0("Q4_",year))
+  
+  #rename weight variable
+  reformat_dataset<-function(dataset){
+  colnames<-c(tail(sort(names(dataset[grep("PIWT", names(dataset))])),1),
+              "Graduate_Type",
+              "AgeGroup",
+              "SEX",
+              "ILODEFR",
+              "FTPTWK",
+              "GRSSWK")
+  
+  dataset<- dataset[,colnames]
+  
+  weight_var<-tail(sort(names(dataset[grep("PIWT", names(dataset))])),1)
+  weight_index<-match(colnames(dataset),weight_var,0)
+  colnames(dataset)[colnames(dataset) %in% weight_var]  <- c("PIWT")
+  return(dataset)
+  }
+  
+  dataset1<-reformat_dataset(dataset1)
+  dataset2<-reformat_dataset(dataset2)
+  dataset3<-reformat_dataset(dataset3)
+  dataset4<-reformat_dataset(dataset4)
+  
+  
+  dataset<-rbind(dataset1, dataset2, dataset3, dataset4)
+  
+   #Restrict to just those in full time employment
+ dataset<-dataset[which(dataset$FTPTWK=="Full-time" & dataset$ILODEFR=="In employment"),]
+ 
+ #find the weight variable for the dataset
+ weight<-tail(sort(names(dataset[grep("PIWT", names(dataset))])),1)
+ 
+  #restrict to just those with an income weight. Those with 0 weight wouldn't be included in the weighted median anyway, but removing at this stage speeds up computation.
+  dataset<-dataset[which(dataset[,weight]!=0),]
+  
+
+  
+  #----Gross Weekly pay in main job----
+ 
+  
+  ##Split by Age, Gender and Graduate Type
+  
+  all_age_sex<-Average_salary_by(dataset,c("AgeGroup","SEX", "Graduate_Type"))
+  
+  ##Split by Age and Graduate Type
+
+  all_age<-Average_salary_by(dataset,c("AgeGroup", "Graduate_Type"))
+  
+  #add extra variable to show this is for any gender
+  all_age$SEX<-"Total"
+  
+  ##Split by Gender and Graduate Type
+  
+  all_sex<-Average_salary_by(dataset,c("SEX", "Graduate_Type"))
+  #add extra variable to show this is for any age
+  all_sex$AgeGroup<-"Total"
+  
+ #Just split by graduate type (i.e. across all agebands)
+  #Use the function just defined above
+  all_total<-  Average_salary_by(dataset,"Graduate_Type")
+  
+#Add in an 'agegroup' for the average weekly salaries by graduate type and rename the column headers: this way we can merge the datasets as they'll have the same headers
+  all_total$AgeGroup<-"Total"
+  all_total$SEX<-"All"
+  
+  #append the average salaries for people by graduate type and ageband with those for all agebands.
+  all_sal<-rbind(all_age, all_total, all_sex , all_age_sex)
+  all_sal$year<-year
+  
+  #rename the young_grad_sal dataset to reference the year and quarter it's run on and output to the global environment.
+  assign(paste0("Sal_",year),all_sal,envir = globalenv())
+  #Also write to an Excel file for ease of use by economics team.
+  #write.csv(all_sal,paste0("Outputs for GLMS/SAL_",year,".csv"))
+}
+
+#-----Graduate Breakdown salaries(16-64) & Most recent year only------
+
+#Restrict to just graduates 
+#rename weight variable
+grad_dat_reform<-function(dataset){
+
+#rename and pick the latest people income weight, select collumns needed  
+  colnames<-c(tail(sort(names(dataset[grep("PIWT", names(dataset))])),1),
+              "Graduate_Type",
+              "AgeGroup",
+              "SEX",
+              "ILODEFR",
+              "FTPTWK",
+              "GRSSWK",
+              "ETHNICITY",
+              "DISEA",
+              "STEM",
+              "LEM",
+              "DEGCLS7",
+              "OSSAH",
+              "Occupation",
+              "INDE07M",
+              "GOVTOF2")
+  
+  dataset<- dataset[,colnames]
+  
+  weight_var<-tail(sort(names(dataset[grep("PIWT", names(dataset))])),1)
+  weight_index<-match(colnames(dataset),weight_var,0)
+  colnames(dataset)[colnames(dataset) %in% weight_var]  <- c("PIWT") 
+  return(dataset)
+}
+
+
+  Graduate_breakdown_salaries<-function(year){
+    
+    #select dataset: for example if quarter =1 and year =2016 this will get Q1_2016 and set this to 'dataset
+    dataset1 <- get(paste0("Q1_",year))
+    dataset2 <- get(paste0("Q2_",year))
+    dataset3 <- get(paste0("Q3_",year))
+    dataset4 <- get(paste0("Q4_",year))
+    
+    #modify datasets using grad_dat_reform func. This selects the most up to date income 
+    #weight and renames it
+    dataset1 <- grad_dat_reform(dataset1)
+    dataset2 <- grad_dat_reform(dataset2)
+    dataset3 <- grad_dat_reform(dataset3)
+    dataset4 <- grad_dat_reform(dataset4)
+    
+    #bind quarterly dataset to make yearly dataset
+    dataset<-rbind(dataset1, dataset2, dataset3, dataset4)
+    
+    #Restrict to just those in full time employment
+    dataset<-dataset[which(dataset$FTPTWK=="Full-time" & dataset$ILODEFR=="In employment"),]
+    
+    #find the weight variable for the dataset
+    weight<-tail(sort(names(dataset[grep("PIWT", names(dataset))])),1)
+    
+    #restrict to just those with an income weight. Those with 0 weight wouldn't be included in the weighted median anyway, but removing at this stage speeds up computation.
+    dataset<-dataset[which(dataset[,weight]!=0),]
+    
+    
+  #restrict to just graduates
+  dataset<-dataset[which(dataset$Graduate_Type=="Graduate"),]
+  
+  #Average salary by ageband
+  age<-Average_salary_by(dataset,"AgeGroup")
+  age<-dplyr::rename(age, Value=AgeGroup)
+
+  #Repeat for average salary by gender
+  sex<-Average_salary_by(dataset,"SEX")
+  sex<-dplyr::rename(sex, Value=SEX)
+  
+  #Average salary by ethnicity
+  ethnicity<-Average_salary_by(dataset,"ETHNICITY")
+  ethnicity<-dplyr::rename(ethnicity, Value=ETHNICITY)
+  
+  #Average salary by disability status
+  
+  disability<-Average_salary_by(dataset,"DISEA")
+  disability<-dplyr::rename(disability, Value=DISEA)
+  
+  #Average salary by degree classification
+  degreeclass<-Average_salary_by(dataset,"DEGCLS7")
+  degreeclass<-dplyr::rename(degreeclass, Value=DEGCLS7)
+  
+  #Average salary by STEM/non-STEM graduates
+  stem<-Average_salary_by(dataset,"STEM")
+  stem<-dplyr::rename(stem, Value=STEM)
+  stem$Value<-as.character(stem$Value)
+  
+  #Average salary by LEM/non-LEM graduates
+  lem<-Average_salary_by(dataset,"LEM")
+  lem<-dplyr::rename(lem, Value=LEM)
+  lem$Value<-as.character(lem$Value)
+
+  #Average salary by OSSAH/non-OSSAH graduates
+  ossah<-Average_salary_by(dataset,"OSSAH")
+  ossah<-dplyr::rename(ossah, Value=OSSAH)
+  ossah$Value<-as.character(ossah$Value)
+  
+  #Average salary by occupational group
+  occupation<-Average_salary_by(dataset,"Occupation")
+  occupation<-dplyr::rename(occupation, Value=Occupation)
+  
+#Average salary by industry category
+  industry<-Average_salary_by(dataset,"INDE07M")
+  industry<-dplyr::rename(industry, Value=INDE07M)
+  
+  #Average salary by region 
+  region<-Average_salary_by(dataset,"GOVTOF2")
+  region<-dplyr::rename(region, Value=GOVTOF2)
+  
+  #Average salary by Sex and industry category
+  industry_sex<-Average_salary_by(dataset,c("SEX","INDE07M"))
+  industry_sex<-dplyr::rename(industry_sex, Value=SEX,Value1=INDE07M)
+  
+  #append datasets to each other to create one dataset of graduate salaries for different demographic characteristics
+      grad_sal<-bind_rows(list(age, 
+                    sex,
+                    ethnicity,
+                    disability, 
+                    degreeclass, 
+                    stem, 
+                    lem,
+                    ossah,
+                    occupation, 
+                    industry,
+                    region,
+                    industry_sex))
+  
+  
+  #rename the grad_sal dataset to reference the year and quarter it's run on and output to the global environment.
+  assign(paste0("grad_sal_",year),grad_sal,envir = globalenv())
+  #Also write to an Excel file for ease of use by economics team.
+  
+  write.csv(grad_sal,paste0(filepath, "Outputs_for_GLMS/GRAD_SAL_",year,".csv"))
+  
+  saveRDS(object = grad_sal,
+          file = paste0(filepath, "Outputs_for_GLMS/EES_rds/grad_sal_", year, ".rds"))
+  
+  
+  #----Graduate Break down Salaries (21-30):young graduates----
+ 
+  dataset<-dataset[which(dataset$Graduate_Type=="Graduate" & dataset$AgeGroup=="21-30"),]
+  
+  #Average salary by ageband
+  age<-Average_salary_by(dataset,"AgeGroup")
+  age<-dplyr::rename(age, Value=AgeGroup)
+  
+  #Repeat for average salary by gender
+  sex<-Average_salary_by(dataset,"SEX")
+  sex<-dplyr::rename(sex, Value=SEX)
+  
+  #Average salary by ethnicity
+  ethnicity<-Average_salary_by(dataset,"ETHNICITY")
+  ethnicity<-dplyr::rename(ethnicity, Value=ETHNICITY)
+  
+  #Average salary by disability status
+  
+  disability<-Average_salary_by(dataset,"DISEA")
+  disability<-dplyr::rename(disability, Value=DISEA)
+  
+  #Average salary by degree classification
+  degreeclass<-Average_salary_by(dataset,"DEGCLS7")
+  degreeclass<-dplyr::rename(degreeclass, Value=DEGCLS7)
+  
+  #Average salary by STEM/non-STEM graduates
+  stem<-Average_salary_by(dataset,"STEM")
+  stem<-dplyr::rename(stem, Value=STEM)
+  stem$Value<-as.character(stem$Value)
+  
+  #Average salary by LEM/non-LEM graduates
+  lem<-Average_salary_by(dataset,"LEM")
+  lem<-dplyr::rename(lem, Value=LEM)
+  lem$Value<-as.character(lem$Value)
+  
+  #Average salary by OSSAH/non-OSSAH graduates
+  ossah<-Average_salary_by(dataset,"OSSAH")
+  ossah<-dplyr::rename(ossah, Value=OSSAH)
+  ossah$Value<-as.character(ossah$Value)
+  
+  #Average salary by occupational group
+  occupation<-Average_salary_by(dataset,"Occupation")
+  occupation<-dplyr::rename(occupation, Value=Occupation)
+  
+  #Average salary by industry category
+  industry<-Average_salary_by(dataset,"INDE07M")
+  industry<-dplyr::rename(industry, Value=INDE07M)
+  
+  #Average salary by region 
+  region<-Average_salary_by(dataset,"GOVTOF2")
+  region<-dplyr::rename(region, Value=GOVTOF2)
+  
+  #Average salary by Sex and industry category
+  industry_sex<-Average_salary_by(dataset,c("SEX","INDE07M"))
+  industry_sex<-dplyr::rename(industry_sex, Value=SEX,Value1=INDE07M)
+  
+  #append datasets to each other to create one dataset of graduate salaries for different demographic characteristics
+      young_grad_sal<-bind_rows(list(age, 
+                          sex, 
+                          ethnicity,
+                          disability,
+                          degreeclass,
+                          stem, 
+                          lem, 
+                          ossah,
+                          occupation, 
+                          industry,
+                          region,
+                          industry_sex))
+  
+  #rename the young_grad_sal dataset to reference the year and quarter it's run on and output to the global environment.
+  assign(paste0("young_grad_sal_",year),young_grad_sal,envir = globalenv())
+  #Also write to an Excel file for ease of use by economics team.
+ 
+  write.csv(young_grad_sal,paste0(filepath, "Outputs_for_GLMS/YOUNG_GRAD_SAL_", year, ".csv"))
+  
+  saveRDS(object = young_grad_sal,
+          file = paste0(filepath, "Outputs_for_GLMS/EES_rds/young_grad_sal_", year, ".rds"))
+  
+  }
+  
+
+#Graduate_breakdown_salaries(2019)
+
---
+++ GLMS_proportion.R
@@ -0,0 +1,176 @@
+#--------Introduction----
+#This scripts finds the what proportion of those that are employed that work part-time(PT) 
+#The PT workers proportion is found for working age Graduates and Non Graduates
+
+#--------Contents----------
+#1. a function that takes modified quarterly data set and finds counts/sample size by graduate type = Graduate and Non Grad
+#   and calculates the proportion of those employed that work part-time(PT)
+#2. Modifies quarterly datasets by selecting the relevant people weight. This is used in pt_proportion_tb
+#   function.
+#3. QA outputs of proportion function
+#4. finds the average proportion of workers that work PT  by graduate type and year
+#5. Summary table of average proportion of graduate types that work part-time for a  year 
+
+#NOTE PT = part-time, FT = full-time, WK = work
+
+#----load library----
+library("reshape2")
+library("rlang")
+
+#----1. Part-time(PT) Proportions data frame ----
+#The all dataset has been modified in step 2 so as to ensure the correct people weight is used
+
+pt_proportion_tb_a<-function(all){
+  
+  #find the sample size and no. of people in each graduate type 
+  all %>%
+    group_by(Graduate_Type)%>% summarise(total=sum(freq),sample=sum(sample))->full_pop
+  
+
+    #Make in_employment table by select those in employment and group by grad type. Then find
+    #sample size etc.
+   
+    all[which(all$ILODEFR=="In employment"),] %>%
+      group_by(Graduate_Type)%>% summarise(count=sum(freq),sample=sum(sample))-> in_employment_FTPTWK
+    
+    
+ 
+    #Make in_employment table for PT workers only by select those in employment and those in PT work, group by grad type. 
+    #Then find sample size etc. 
+    
+    all[which(all$ILODEFR=="In employment" & all$FTPTWK == "Part-time"),] %>%
+      group_by(Graduate_Type)%>% summarise(count=sum(freq),sample=sum(sample)) ->in_employment_PTWK
+    
+    #Make a data frame for employed stats
+    pt_proportion_a<-data.frame( in_employment_FTPTWK$Graduate_Type,                              #names of graduate_type for each grad type
+                                in_employment_PTWK$count,                                    #all employed sample size
+                                in_employment_FTPTWK$count,                                     #all employed count
+                                in_employment_PTWK$sample,                                      #PT workers sample size
+                                in_employment_FTPTWK$sample,                                       #PT workers count
+                                in_employment_PTWK$count/in_employment_FTPTWK$count)            # proportion of PT workers that are in the employed population                                                                 #full_population by grad type
+  
+#collumn names
+    colnames(pt_proportion_a)<-c("graduate_type",
+                                "employed-total-PT","employed-total-FTPT",
+                                "employed-sample-PT","employed-sample-FTPT",
+                                "pt_employed_proportion")
+    return(pt_proportion_a)
+    
+
+  }
+
+#----2. pt_proportion function----
+#Employment_rate function makes datasets  (all) which are used pt_proportion_tb function
+#the most recent people weight is picked and samples/counts for defined population is found
+
+pt_proportion_a<-function(quarter, year){
+
+  #get the dataset from the global environment
+    dataset<-get(paste0("Q",quarter,"_",year))
+    
+  #find the weight variable and just include people that have identified themselves 
+  #with a working pattern
+    weight<-tail(sort(names(dataset[grep("PWT", names(dataset))])),1)
+    dataset<-dataset[which(dataset[,weight]>0 & !is.na(dataset$FTPTWK)),]
+  
+  # count all people by age, graduate type, ILO employment status and SOC group: weight people by the weight variable  (such as PWT16)
+  #also note sample size
+  all<-dataset%>%
+    group_by(AgeGroup,Graduate_Type,ILODEFR,SOCHE,FTPTWK) %>%
+    dplyr::summarise(freq=sum(!!sym(weight)), sample=n())
+  
+  #take all dataset and output a new dataframe using pt_proportion function
+  pt_proportion<-pt_proportion_tb_a(all)
+  
+  #save dataframe part-time proportions to global environment
+  assign(paste0("pt_proportion_Q",quarter,"_",year),pt_proportion,envir = globalenv())
+
+  
+}
+
+#Range of quarterly datasets 
+sapply(start_year:end_year, function(y)sapply(1:4, function(x)pt_proportion_a(x,y)))
+
+#3. ----QA: quarterly datasets-----
+#Things to check include:
+#Are the sample sizes large enough ? Historically yearly data sets should have a sample size of >=31. Decide that quarterly Part-part-time proportions
+# Check if quarterly data have samples sizes >=31
+
+QA_quart_proportion <- function(quarter,year){
+  
+  dat <- get(paste0("pt_proportion_Q",quarter,"_",year)) %>%
+    filter (graduate_type == "Graduate" | graduate_type == "Non Grad") # Just select Grads and Non Grads
+  
+  
+  #check all quarterly data sets for zeros and NA's and that sample sizes are >=31
+  print(ifelse(all(!is.na(dat)) & all(dat[,4:5] != 0) & all(dat[,4:5] >= 31),
+         paste0("pt_proportion_Q",quarter,"_",year," has passed QA checks"),
+         paste0("Warning: pt_proportion_Q",quarter,"_",year," has not passed QA checks")))
+  
+}
+
+#outputs for Qa
+sapply(start_year:end_year, function(y)sapply(1:4, function(x)QA_quart_proportion (x,y)))
+
+
+
+
+
+ 
+#3.Headline stats:yearly dataset-------------------------
+
+#quarterly datasets combined to yearly datasets and mean rates and sample sizes, counts found
+  year_average_pt_tb<-function(year){
+  
+  #select dataset: for example if quarter =1 and year =2016 this will get Q1_2016 and set this to 'dataset'
+  dataset1<-get(paste0("pt_proportion_Q1_",year))
+  dataset2<-get(paste0("pt_proportion_Q2_",year))
+  dataset3<-get(paste0("pt_proportion_Q3_",year))
+  dataset4<-get(paste0("pt_proportion_Q4_",year))
+  
+  year_data_proportion<-rbind(dataset1,dataset2,dataset3,dataset4)%>%
+    filter (graduate_type == "Graduate" | graduate_type == "Non Grad")       #filter for Graduate amd Non Grads
+  
+  
+  #group by age range and graduate type (i.e population), summarise the statistics for 
+  #the year
+  
+  year_data_proportion%>%
+    group_by(graduate_type)%>%summarise(pt_employed_proportion = mean.default(pt_employed_proportion),
+    employed_PT_sample=sum(`employed-sample-PT`),
+    employed_PT_total=sum(`employed-total-PT`),
+    employed_FTPT_sample=sum(`employed-sample-FTPT`), 
+    employed_FTPT_total=sum(`employed-total-FTPT`)) -> year_average_pt_proprtion
+              
+              
+  
+  
+  #label the year_avearge table with year
+  year_average_pt_proprtion$time_period<-year
+  
+  assign(paste0("pt_proportion_",year),year_average_pt_proprtion,envir = globalenv())
+  
+  
+}
+sapply(start_year:end_year,function(x) year_average_pt_tb(x))
+
+#4. ----Summary table pt proportions for Grads and non grads by year-----
+#Make a timeseries for pt proportions Grads and non grads 
+list_timeseries_pt_prop <-lapply(start_year:end_year,function(x)get(paste0("pt_proportion_",x)))
+timeseries_pt_proportion <- Reduce(rbind,list_timeseries_pt_prop) %>%
+  mutate(Population = "AgeGroup")%>%
+  mutate(Breakdown = "16-64")
+
+saveRDS(object = timeseries_pt_proportion,
+        file = paste0(filepath, "Outputs_for_GLMS/EES_rds/timeseries_pt_proportion_",start_year,"_",end_year,".rds"))
+
+
+#4.1 ----QA summary table----
+#Check for NA's and zeros in the table
+print("Are there any zeros or Na's in the summary table ")
+print(apply(timeseries_pt_proportion, 2, function(x) any((x==0))))
+print(apply(timeseries_pt_proportion, 2, function(x) any(is.na(x))))
+
+
+  
+
